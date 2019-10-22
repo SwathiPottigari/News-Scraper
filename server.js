4fc5410ef5707118088223859d865a43d6d0515d
Swathi <pottigariswathi@gmail.com>
2019-10-21 22:11:36 -0700

    Added the views and changed the folder structure

--- server.js
+++ server.js
@@ -5,7 +5,13 @@ var cheerio = require("cheerio");
 
 var app=express();
 
+// var db=require("./models");
+var routes=require("./routes/htmlRoutes");
+
 var PORT=process.env.PORT||3000;
+
+var MONGODB_URI=process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.connect(MONGODB_URI);
 
 // This converts the express response to the JSON format
 app.use(express.urlencoded({extended:true}));
@@ -13,6 +19,9 @@ app.use(express.json());
 
 // It uses the static file path when deployed in HEROKU
 app.use(express.static("public"));
+
+// This uses the routes folder files for routing
+app.use("/",routes);
 
 // Starts the server
 app.listen(PORT,function(){
