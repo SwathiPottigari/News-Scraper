0f2155f50d6c72b8efe2bab1a2c2fa87f309b57e
Swathi <pottigariswathi@gmail.com>
2019-10-27 17:54:01 -0700

    Added the Save Notes functionality

--- public/assets/js/newsScraper.js
+++ public/assets/js/newsScraper.js
@@ -1,21 +1,21 @@
-$(function(){
+$(function () {
 
-    $(".save").click(function(event){
-        let id=$(this).parent().parent().parent().attr("data-id");
-        $.ajax("/saveArticle/"+id,{
-            type:"PUT"
-        }).then(function(){
+    $(".save").click(function (event) {
+        let id = $(this).parent().parent().parent().attr("data-id");
+        $.ajax("/saveArticle/" + id, {
+            type: "PUT"
+        }).then(function () {
             location.reload();
         }).catch();
     });
 
-    $(".delete").click(function(event){
-        let id=$(this).parent().parent().parent().attr("data-id");
-        $.ajax("/deleteArticle/"+id,{
-            type:"DELETE"
-        }).then(function(result){
+    $(".delete").click(function (event) {
+        let id = $(this).parent().parent().parent().attr("data-id");
+        $.ajax("/deleteArticle/" + id, {
+            type: "DELETE"
+        }).then(function (result) {
             location.reload();
-        }).catch(function(error){
+        }).catch(function (error) {
             console.log(error);
         });
     });
@@ -32,13 +32,27 @@ $(function(){
     //     });
     // });
 
-    $(".note-delete").click(function(event){
-        let notesId=$(this).attr('data-id');
-        let articleId=$(this).closest(".modal").attr("id").split("-")[1];
-        $.ajax("/deleteNotes/"+notesId+"/"+articleId,{
-            type:"DELETE"
-        }).then(function(results){
+    $(".note-delete").click(function (event) {
+        let notesId = $(this).attr('data-id');
+        let articleId = $(this).closest(".modal").attr("id").split("-")[1];
+        $.ajax("/deleteNotes/" + notesId + "/" + articleId, {
+            type: "DELETE"
+        }).then(function (results) {
             location.reload();
-        }).catch(function(error){console.log(error)});
+        }).catch(function (error) { console.log(error) });
+    });
+
+    $(".saveNotes").click(function (event) {
+        let id = $(this).closest(".modal").attr("id").split("-")[1];
+        let notes={
+            note:$("#newNotes").val()
+        };
+        
+        $.ajax("/saveNotes/" + id,{
+            type:"POST",
+            data:notes
+        }).then(function (results) {
+            location.reload();   
+        }).catch(function (error) { console.log(error) });
     });
 });
